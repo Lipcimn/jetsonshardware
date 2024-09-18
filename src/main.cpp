473c0a4f84b0e543c3902a2bbc6f410963c9a9b6
Lipcimn <eduardoguicorrea@gmail.com>
2024-09-18 19:55:16 -0300

    initial functionality setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,80 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <stdint.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* Pins Definition:
+ * 1, 2, 3, 4 -> LEDs
+ * 12 -> Proximity Sensor
+ * 13 -> Servo
+ */
+const uint8_t led[4] = {10, 11, 12, 13};
+const uint8_t proximitySensor = 14;
+const uint8_t servoPin = 13;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Servo servo;
+
+TFT_eSPI tft = TFT_eSPI();
+
+// Variables
+unsigned long ledInterval = millis();
+unsigned long servoInterval = millis();
+
+/**
+ * Function that controls the LEDs
+ * @param milliseconds: The time in milliseconds that the LEDs will be on
+ */
+void ledFunctionality(uint8_t milliseconds)
+{
+  if (millis() - ledInterval == milliseconds)
+  {
+    for (int i = 0; i < 4; i++)
+      digitalWrite(led[i], HIGH);
+
+    ledInterval = millis();
+  }
+  else
+  {
+    for (int i = 0; i < 4; i++)
+      digitalWrite(led[i], LOW);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**
+ * Function that controls the Servo
+ * @param milliseconds: The time in milliseconds that the Servo will be on
+ */
+void servoFunctionality(uint8_t milliseconds)
+{
+  if (millis() - servoInterval == milliseconds)
+  {
+    servo.write(180);
+    servoInterval = millis();
+  }
+  else
+  {
+    servo.write(0);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+  servo.attach(servoPin);
+
+  pinMode(proximitySensor, INPUT);
+  for (int i = 0; i < 4; i++)
+    pinMode(led[i], OUTPUT);
+
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE);
+  tft.setCursor(0, 0);
+}
+
+void loop()
+{
+  ledFunctionality(500);
+  servoFunctionality(5000);
 }
