f6a7f1ccf9cb2f5115ac3e7b37a787d472c7c1d7
Lipcimn <eduardoguicorrea@gmail.com>
2024-10-16 12:03:02 -0300

    refactor: changed buttons size and positions

--- src/classes/classes.cpp
+++ src/classes/classes.cpp
@@ -4,13 +4,13 @@ Servo servo;
 
 TFT_eSPI display;
 
-DisplayBT ButtonLED1(5, 5, 50, 50, "LED1");
-DisplayBT ButtonLED2(65, 5, 50, 50, "LED2");
-DisplayBT ButtonLED3(125, 5, 50, 50, "LED3");
-DisplayBT ButtonLED4(185, 5, 50, 50, "LED4");
-DisplayBT ButtonLED5(5, 65, 50, 50, "LED5");
-DisplayBT ButtonLED6(65, 65, 50, 50, "LED6");
-DisplayBT ButtonSERVO(5, 185, 50, 50, "SERVO");
+DisplayBT ButtonLED1(5, 5, 60, 60, "LED1");
+DisplayBT ButtonLED2(75, 5, 60, 60, "LED2");
+DisplayBT ButtonLED3(145, 5, 60, 60, "LED3");
+DisplayBT ButtonLED4(5, 75, 60, 60, "LED4");
+DisplayBT ButtonLED5(75, 75, 60, 60, "LED5");
+DisplayBT ButtonLED6(145, 75, 60, 60, "LED6");
+DisplayBT ButtonSERVO(240, 40, 60, 60, "SERVO");
 DisplayBT Buttons[7] = {ButtonLED1, ButtonLED2, ButtonLED3, ButtonLED4, ButtonLED5, ButtonLED6, ButtonSERVO};
 
 DisplayBT lastTouchedButton(0, 0, 0, 0, "NULL"); // This instance is used to negate the effect of the user pressing the same button indefinitely.
