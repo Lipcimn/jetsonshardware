9ca1e06cc20c2069f0372c38e2a130bea7244892
Lipcimn <eduardoguicorrea@gmail.com>
2024-10-23 11:43:59 -0300

    refactor: change DiplayBT state prop type

--- src/classes/DisplayBT/DisplayBT.h
+++ src/classes/DisplayBT/DisplayBT.h
@@ -8,7 +8,7 @@ class DisplayBT
 {
 public:
     uint8_t coordX, coordY, width, height;
-    uint8_t state = 0;
+    bool state = 0;
     const char *label;
     DisplayBT(unsigned int x, unsigned int y, uint8_t w, uint8_t h, const char *l);
     friend bool operator == (DisplayBT a, DisplayBT b);
