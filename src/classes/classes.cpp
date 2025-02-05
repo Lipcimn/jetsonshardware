4ad0c3cc3739f71558ca61881738db835a68d794
Lipcimn <eduardoguicorrea@gmail.com>
2025-02-05 14:43:56 -0300

    style: change label inside buttons

--- src/classes/classes.cpp
+++ src/classes/classes.cpp
@@ -4,13 +4,13 @@ Servo servo;
 
 TFT_eSPI display;
 
-DisplayBT ButtonLED1(5, 5, 60, 60, "LED1");
-DisplayBT ButtonLED2(75, 5, 60, 60, "LED2");
-DisplayBT ButtonLED3(145, 5, 60, 60, "LED3");
-DisplayBT ButtonLED4(5, 75, 60, 60, "LED4");
-DisplayBT ButtonLED5(75, 75, 60, 60, "LED5");
-DisplayBT ButtonLED6(145, 75, 60, 60, "LED6");
-DisplayBT ButtonSERVO(240, 5, 60, 60, "SERVO");
+DisplayBT ButtonLED1(5, 5, 60, 60, "COZNH");
+DisplayBT ButtonLED2(75, 5, 60, 60, "JANTA");
+DisplayBT ButtonLED3(145, 5, 60, 60, "ESCRI");
+DisplayBT ButtonLED4(5, 75, 60, 60, "SALA");
+DisplayBT ButtonLED5(75, 75, 60, 60, "BANHO");
+DisplayBT ButtonLED6(145, 75, 60, 60, "GARAG");
+DisplayBT ButtonSERVO(240, 5, 60, 60, "PORTA");
 DisplayBT ButtonALARM(240, 75, 60, 60, "ALARM");
 DisplayBT Buttons[8] = {ButtonLED1, ButtonLED2, ButtonLED3, ButtonLED4, ButtonLED5, ButtonLED6, ButtonSERVO, ButtonALARM};
 
